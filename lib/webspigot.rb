605af65f88908e877f8e4f2f09909cc804b54837
Shane Thomas <shane@devshane.com>
2014-02-16 00:58:22 -0500

    config, better compositing

--- lib/webspigot.rb
+++ lib/webspigot.rb
@@ -1,52 +1,95 @@
 require 'mechanize'
 require 'open-uri'
+require 'tempfile'
 
 class Webspigot
   attr_accessor :search_phrase, :image_url
 
-  def initialize
+  def initialize(options)
+    @options = options
     @m = Mechanize.new
-
-    @options = {
-      safe_mode: "OFF"
-    }
   end
 
   def run(phrase=nil)
     if phrase.nil?
       phrases = []
-      @m.get('https://news.google.com/') do |page|
+
+      # b  - Business
+      # w  - World
+      # n  - US
+      # tc - Technology
+      # e  - Entertainment
+      # s  - Sports
+      # snc- Science
+      # m  - Health
+      # ir - Spotlight
+      section = ['b', 'w', 'n', 'tc', 'e', 's', 'snc', 'm', 'ir']
+      url = "https://news.google.com/news/section/?section=#{section.sample}"
+
+      log "GET #{url}"
+      @m.get(url) do |page|
+        log "got #{url}"
         page.links.each do |link|
-          phrases << link.text if link.text.split.length > 4
+          if link.text.split.length > 4
+            phrases << link.text
+          end
         end
       end
       clean_phrases(phrases)
       phrase = phrases.sample
     end
     @search_phrase = phrase
-    @image_url = get_image_url(phrase)
+    @image_url = nil
+    while @image_url.nil?
+      @image_url = get_image_url(phrase)
+    end
+  end
+
+  def save
+    log "image_url: #{@image_url}"
+    iu = @image_url['?'] ? @image_url[0..@image_url.index('?') - 1] : @image_url
+    #puts "iu: #{iu}"
+    ext = iu[iu.rindex('.')..-1]
+    rnd = (100000 + (99999 * rand)).to_i
+    fname = "/tmp/spigot-#{rnd}#{ext}"
+    #puts "fname: #{fname}"
+    File.open(fname, 'wb') do |f|
+      open(@image_url) do |image|
+        f.write(image.read)
+      end
+    end
+    fname
   end
 
   private
 
   def get_image_url(phrase)
+    log "searching '#{phrase}'"
     enc = URI::encode(phrase)
     links = []
     @m.cookie_jar.add(HTTP::Cookie.new('SRCHHPGUSR', "ADLT=#{@options[:safe_mode]}",
                                        domain: '.bing.com', path: '/'))
-    @m.get("http://www.bing.com/images/search?q=#{enc}&qft=+filterui:imagesize-medium") do |page|
+    @m.get("http://www.bing.com/images/search?q=#{enc}") do |page|
       page.body.scan(%r{,imgurl:&quot;(.*?)&quot;}).each do |thing|
-        links << thing[0].gsub(/"/, '')
+        t = thing[0]
+        if t['.']
+          links << thing[0].gsub(/"/, '')
+        end
       end
     end
     links.sample
   end
 
   def clean_phrases(phrases)
+    log "cleaning #{phrases.length} phrases"
     phrases.each do |p|
       p.gsub!(/\[.*?\]/, '')
       p.gsub!(/\(.*?\)/, '')
       p.gsub!(/\s+/, ' ')
     end
+  end
+
+  def log(what)
+    puts "<#{caller_locations(1,1)[0].label}> #{what}"
   end
 end
